242e91790dee5c3023fe22a18e7efeb51c8afe20
Lin <i@linlin.me>
2023-05-31 12:48:59 +0800

    init

---
+++ Program.fs
@@ -0,0 +1,8 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello from F#"
+    Console.WriteLine("Press any key to exit...")
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
